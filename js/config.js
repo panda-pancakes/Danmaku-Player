1deb05967666b5d1f26f72b40c4d608e027765ca
Hikoyan <huanghongyan324@gmail.com>
2019-04-14 14:41:10 +0800

    png

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 $(function (){
     var name = $("#video_name").val();
-    // var src1 = "http://vodkgeyttp8.vod.126.net/cloudmusic/7ee3/mv/a104/181251ac9d84d0f40c5fa2d8fc263e7e.mp4?wsSecret=002f44c3adb92a5bf6430859fb524b75&wsTime=1554720592";
-    // $("video").attr("src",src1);
-    // $("video").load();//没有用
+    var src1 = "http://vodkgeyttp8.vod.126.net/cloudmusic/7ee3/mv/a104/181251ac9d84d0f40c5fa2d8fc263e7e.mp4?wsSecret=002f44c3adb92a5bf6430859fb524b75&wsTime=1554720592";
+    $("video").attr("src",src1);
+    // $("video").play();
     $("#video_name").text("SEARCH");
     // var viedo_src = $("#video").attr("src","video/");
     // var danmaku;
---
+++ php/config.php
@@ -0,0 +1,21 @@
+<?php
+/* The configuration sample */
+/* Deployment:
+ * 1. Edit this config file.
+ * 2. Rename this file to `config.php`.
+ * 3. Run `install.php`.
+ */
+
+/** The name of the database */
+define('DB_NAME', 'bbs');
+
+/** MySQL database username */
+define('DB_USER', 'blog');
+
+/** MySQL database password */
+define('DB_PASSWORD', '1233211234567');
+
+/** MySQL hostname */
+
+define('DB_HOST', '203.195.221.189');
+
